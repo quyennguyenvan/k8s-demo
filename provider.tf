5cbb72244fba2d8cb3e65499c85566b8641f9ab7
quyen nguyen <joe.nguyen@transwap.com>
2022-07-04 17:28:22 +0700

    fix forget the variable

--- provider.tf
+++ provider.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-  version = "~> 3.15.0"
-  region  = "ap-southeast-1"
-  profile = "joejoe"
+  version    = "~> 3.15.0"
+  region     = "ap-southeast-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 provider "http" {}
